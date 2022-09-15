d7f7dc01b8f71683210b2ce8886178ef6700cffc
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-09-15 14:17:13 +0100

    Initial Changes

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdOutlineVideocamOff } from "react-icons/md";
 import { BiCommentX } from "react-icons/bi";
+import { MainFooter } from "./index";
 
 interface IProps {
   text: string;
@@ -8,12 +9,23 @@ interface IProps {
 
 const NoResults = ({ text }: IProps) => {
   return (
-    <div className="flex flex-col justify-center items-center h-full w-full">
-      <p className="text-8xl">
-        {text === "No comments yet" ? <BiCommentX /> : <MdOutlineVideocamOff />}
-      </p>
-      <p className="text-2xl text-center">{text}</p>
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center h-full w-full">
+        <p className="text-8xl">
+          {text === "No comments yet" ? (
+            <BiCommentX />
+          ) : (
+            <MdOutlineVideocamOff />
+          )}
+        </p>
+        <p className="text-2xl text-center">{text}</p>
+      </div>
+      <div className="absolute bottom-0 w-full cursor-pointer sm:block md:hidden">
+        <div className="h-[3rem]  bg-black border-t-2 border-orange-200 text-gray-50">
+          <MainFooter />
+        </div>
+      </div>
+    </>
   );
 };
 
