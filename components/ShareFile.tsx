de3cbf79b2e00cbdd1bb022cbe6d9a8711dfcd22
D-coder <chuksjoshuaa@gmail.com>
2022-09-17 10:11:58 +0100

    Prettier format code fixed

--- components/ShareFile.tsx
+++ components/ShareFile.tsx
@@ -12,97 +12,109 @@ import {
   LinkedinShareButton,
   LinkedinIcon,
   PinterestShareButton,
-  PinterestIcon
-} from 'next-share';
+  PinterestIcon,
+} from "next-share";
 
 interface IProps {
   setShowShare: React.Dispatch<React.SetStateAction<boolean>>;
-  getUrl: string
-  count: number
+  getUrl: string;
+  count: number;
   setCount: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const ShareFile = ({setShowShare, getUrl, setCount, count}: IProps) => {
+const ShareFile = ({ setShowShare, getUrl, setCount, count }: IProps) => {
+  let result = `${BASE_URL}`;
 
-    let result = `${BASE_URL}`
-   
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-const increment = () => {
-    setCount(count + 1)
-}
+  const urlDecoded = (info: string) => {
+    const data = decodeURI(info);
+    return data;
+  };
 
-const urlDecoded = (info: string) => {
-  const data = decodeURI(info);
-  return data;
-};
-    
-    return (
-        <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-            
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                  <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white " onClick={() => setShowShare(false)}>
-                      <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path></svg>
-                      <span className="sr-only ">Close modal</span>
-                  </button>
-                  <div className="p-6 text-center mt-5">
-                      <p>Share to</p>
-                       <div className="border-t-2 border-gray-900 pt-3 flex m-2 flex-wrap" onClick={increment}>
-                         <div className="pr-3">
-                            <FacebookShareButton
-                                url={` ${result}`}
-                                quote={'next-share is a social share buttons for your next React apps.'}
-                                hashtag={'#video'}
-                                
-                                >
-                                <FacebookIcon size={32} className="rounded-full" />
-                            </FacebookShareButton>
-                         </div>
-                         <div className="pr-3">
-                            <WhatsappShareButton
-                                url={`${result}`}
-                                title={'Watch video from TikTik by clicking this link'}    
-                                >
-                                <WhatsappIcon size={32} className="rounded-full" />
-                            </WhatsappShareButton>
-                         </div>
-                         <div className="pr-3">
-                            <TwitterShareButton
-                                url={` ${result}`}
-                                title={'Watch video from TikTik by clicking this link.'}                              
-                                >
-                                <TwitterIcon size={32} className="rounded-full" />
-                            </TwitterShareButton>
-                         </div>
-                         <div className="pr-3">
-                            <TelegramShareButton
-                                url={`${result}`}
-                                title={'Watch video from TikTik by clicking this link.'}
-                                >
-                                <TelegramIcon size={32} className="rounded-full" />
-                            </TelegramShareButton>
-                         </div>
-                         <div className="pr-3">
-                            <LinkedinShareButton
-                               
-                                url={`${result}`} 
-                                title={'Watch video from TikTik by clicking this link.'}
-                                >
-                                <LinkedinIcon size={32} className="rounded-full" />
-                            </LinkedinShareButton>
-                         </div>
-                         <div className="pr-3">
-                            <PinterestShareButton
-                                url={`${result}`}
-                                media={'Watch video from TikTik by clicking this link.'}
-                                >
-                                <PinterestIcon size={32} className="rounded-full" />
-                            </PinterestShareButton>
-                         </div>
-                       </div>
-                  </div>
-              </div>
+  return (
+    <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+      <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <button
+          type="button"
+          className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white "
+          onClick={() => setShowShare(false)}
+        >
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+          </svg>
+          <span className="sr-only ">Close modal</span>
+        </button>
+        <div className="p-6 text-center mt-5">
+          <p>Share to</p>
+          <div
+            className="border-t-2 border-gray-900 pt-3 flex m-2 flex-wrap"
+            onClick={increment}
+          >
+            <div className="pr-3">
+              <FacebookShareButton
+                url={` ${result}`}
+                quote={
+                  "next-share is a social share buttons for your next React apps."
+                }
+                hashtag={"#video"}
+              >
+                <FacebookIcon size={32} className="rounded-full" />
+              </FacebookShareButton>
+            </div>
+            <div className="pr-3">
+              <WhatsappShareButton
+                url={`${result}`}
+                title={"Watch video from TikTik by clicking this link"}
+              >
+                <WhatsappIcon size={32} className="rounded-full" />
+              </WhatsappShareButton>
+            </div>
+            <div className="pr-3">
+              <TwitterShareButton
+                url={` ${result}`}
+                title={"Watch video from TikTik by clicking this link."}
+              >
+                <TwitterIcon size={32} className="rounded-full" />
+              </TwitterShareButton>
+            </div>
+            <div className="pr-3">
+              <TelegramShareButton
+                url={`${result}`}
+                title={"Watch video from TikTik by clicking this link."}
+              >
+                <TelegramIcon size={32} className="rounded-full" />
+              </TelegramShareButton>
+            </div>
+            <div className="pr-3">
+              <LinkedinShareButton
+                url={`${result}`}
+                title={"Watch video from TikTik by clicking this link."}
+              >
+                <LinkedinIcon size={32} className="rounded-full" />
+              </LinkedinShareButton>
+            </div>
+            <div className="pr-3">
+              <PinterestShareButton
+                url={`${result}`}
+                media={"Watch video from TikTik by clicking this link."}
+              >
+                <PinterestIcon size={32} className="rounded-full" />
+              </PinterestShareButton>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default ShareFile;
--- components/SideIcon.tsx
+++ components/SideIcon.tsx
@@ -1,26 +1,21 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { FaRegCommentDots } from "react-icons/fa";
 import { IoArrowRedoOutline } from "react-icons/io5";
 import { MdFavorite } from "react-icons/md";
 import { ShareFile } from "./index";
 
-
 interface IState {
   showShare: boolean;
 }
 
-const SideIcon = ({getUrl}: any ) => {
-
-  
- 
-  const [showShare, setShowShare] =  useState<IState["showShare"]>(false);
-
-  const [count, setCount] = useState<number>(0)
+const SideIcon = ({ getUrl }: any) => {
+  const [showShare, setShowShare] = useState<IState["showShare"]>(false);
+  const [count, setCount] = useState<number>(0);
 
   const shareButton = () => {
     setShowShare(true);
-  }
-  
+  };
+
   return (
     <div className="flex flex-col gap-5 aligns-center font-bold">
       <div className="mt-2 mb-2 text-4xl  text-white hover:text-gray-400">
@@ -30,16 +25,28 @@ const SideIcon = ({getUrl}: any ) => {
         <div className="mb-8">
           <FaRegCommentDots />
         </div>
-        <div className="mb-8" onClick={shareButton}  data-modal-toggle="popup-modal">
+        <div
+          className="mb-8"
+          onClick={shareButton}
+          data-modal-toggle="popup-modal"
+        >
           <div className="flex flex-col pb-0 p-0 m-0">
             <IoArrowRedoOutline />
-              <div className="text-white text-lg font-bold pl-3 mt-[-0.5rem]">{count}</div>
+            <div className="text-white text-lg font-bold pl-3 mt-[-0.5rem]">
+              {count}
+            </div>
           </div>
-         
         </div>
       </div>
       <div className="overflow-y-auto overflow-x-hidden fixed top-20 mt-20 right-0 left-0 z-50 md:inset-0 h-modal">
-        {showShare && <ShareFile setShowShare={setShowShare} getUrl={getUrl} setCount={setCount} count={count} />}
+        {showShare && (
+          <ShareFile
+            setShowShare={setShowShare}
+            getUrl={getUrl}
+            setCount={setCount}
+            count={count}
+          />
+        )}
       </div>
     </div>
   );
