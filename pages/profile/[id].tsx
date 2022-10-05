6c4918e40b7fcea15badce5b0ed7a1176843c4c3
Chukwudi Joshua <chuksjoshuaa@gmail.com>
2022-10-05 09:20:26 +0100

    Update [id].tsx

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,112 +1,119 @@
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { GoVerified } from "react-icons/go";
-import axios from "axios";
-import { ProfileVideoCard, NoResults, MainFooter } from "../../components";
-import { IUser, Video } from "../../types";
+import React, { useState, useEffect } from "react";
 import { BASE_URL } from "../../utils";
+import axios from "axios";
+import Link from "next/link";
+import Image from "next/image";
+import { GoVerified } from "react-icons/go";
+import { VideoCard, NoResults } from "../../components";
+import { IUser, Video } from "../../types";
+import { useRouter } from "next/router";
+import useAuthStore from "../../store/authStore";
 
-interface IProps {
-  data: {
-    user: IUser;
-    userVideos: Video[];
-    userLikedVideos: Video[];
-  };
-}
+const Search = ({ videos }: { videos: Video[] }) => {
+  const Router = useRouter();
+  const [isAccounts, setIsAccounts] = useState(false);
+  const { allUsers } = useAuthStore();
 
-const Profile = ({ data }: IProps) => {
-  const { user, userVideos, userLikedVideos } = data;
-  const [videosList, setVideosList] = useState<Video[]>([]);
-  const [showUserVideos, setShowUserVideos] = useState(true);
-  const videos = showUserVideos ? "border-b-2 border-black" : "text-gray-400";
-  const liked = !showUserVideos ? "border-b-2 border-black" : "text-gray-400";
+  //To get the word that was searched
+  const { searchTerm }: any = Router.query;
+  const showUserVideos = false;
 
-  useEffect(() => {
-    if (showUserVideos) {
-      setVideosList(userVideos);
-    } else {
-      setVideosList(userLikedVideos);
-    }
-  }, [showUserVideos, userLikedVideos, userVideos]);
+  const searchAccounts = allUsers.filter((user: IUser) =>
+    user.userName.toLowerCase().includes(searchTerm?.toLowerCase())
+  );
+
+  const accounts = isAccounts ? "border-b-2 border-black" : "text-gray-400";
+  const isVideos = !isAccounts ? "border-b-2 border-black" : "text-gray-400";
 
   return (
-    <>
-      <div className="w-full">
-        <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
-          <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded pt-2">
-            <div className="w-8 h-16 md:w-32 md:h-32">
-              <Image
-                src={user?.image}
-                width={34}
-                height={34}
-                className="rounded-full"
-                alt="user profile"
-                layout="responsive"
-              />
-            </div>
-
-            <div
-              className="flex flex-col justify-center"
-              style={{ marginTop: "-1.5em" }}
-            >
-              <p className="md:text-2xl tracking-wider flex gap-1 items-center justify-center text-md font-bold text-primary lowercase">
-                {user?.userName.replaceAll(" ", "")}
-                <GoVerified className="text-blue-400" />
-              </p>
-              <p className="capitalize md:text-xl text-gray-400 text-xs">
-                {user.userName}
-              </p>
-            </div>
-          </div>
+    <div className="w-full">
+      <div>
+        <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
+          <p
+            className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`}
+            onClick={() => setIsAccounts(true)}
+          >
+            Accounts
+          </p>
+          <p
+            className={`text-xl font-semibold cursor-pointer mt-2 ${isVideos}`}
+            onClick={() => setIsAccounts(false)}
+          >
+            Videos
+          </p>
         </div>
+        {isAccounts ? (
+          <div className="md:mt-16">
+            {searchAccounts.length > 0 ? (
+              searchAccounts.map((user: IUser) => (
+                <Link href={`/profile/${user._id}`} key={user?._id}>
+                  <a>
+                    <div className="flex p-2 cursor-pointer font-semibold rounded  gap-3 w-[100vw] md:w-[70vw] lg:w-[40vw]">
+                      <div>
+                        <Image
+                          src={user?.image}
+                          width={50}
+                          height={50}
+                          className="rounded-full"
+                          alt="user profile"
+                        />
+                      </div>
 
-        <div className="">
-          <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full ">
-            <p
-              className={`text-xl font-semibold cursor-pointer mt-2 ml-4 md:ml-0 ${videos}`}
-              onClick={() => setShowUserVideos(true)}
-            >
-              Videos
-            </p>
-            <p
-              className={`text-xl font-semibold cursor-pointer mt-2 ${liked}`}
-              onClick={() => setShowUserVideos(false)}
-            >
-              Liked
-            </p>
-          </div>
-
-          <div className="flex gap-6 flex-wrap md:justify-center flex-col px-3 md:px-0">
-            {videosList.length > 0 ? (
-              videosList.map((post: Video, idx: number) => {
-                return <ProfileVideoCard post={post} key={idx} />;
-              })
+                      <div className="mb-4">
+                        <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                          {user.userName.replaceAll(" ", "")}
+                          <GoVerified className="text-blue-400" />
+                        </p>
+                        <p className="capitalize text-gray-400 text-xs">
+                          {user.userName}
+                        </p>
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              ))
             ) : (
               <div className="w-[100vw] md:w-[70vw] lg:w-[40vw]">
-                <NoResults
-                  text={`No ${showUserVideos ? "" : "Liked"} Videos Yet `}
-                  showUserVideos={showUserVideos}
-                />
+                 <NoResults
+                    text={`No account associated with ${searchTerm}`}
+                    showUserVideos={showUserVideos}
+                  />
               </div>
             )}
           </div>
-        </div>
+        ) : (
+          <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start flex-col">
+            {videos.length ? (
+              videos.map((video: Video, idx) => (
+                <div key={idx} className="w-[100vw] md:w-[70vw] lg:w-[40vw]">
+                  <VideoCard post={video} key={idx} />
+                </div>
+              ))
+            ) : (
+              <div className="w-[100vw] md:w-[70vw] lg:w-[40vw]">
+                 <NoResults
+                    text={`No video results for ${searchTerm}`}
+                    showUserVideos={showUserVideos}
+                  />
+              </div>
+            )}
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
 export const getServerSideProps = async ({
-  params: { id },
+  params: { searchTerm },
 }: {
-  params: { id: string };
+  params: { searchTerm: string };
 }) => {
-  const res = await axios.get(`${BASE_URL}/api/profile/${id}`);
+  const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
 
   return {
-    props: { data: res.data },
+    props: { videos: res.data },
   };
 };
 
-export default Profile;
+export default Search;
